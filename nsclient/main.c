aa8587cd4f6ee0913e72d30526d417a7f6f495bd
iristaub <59415717+iristaub@users.noreply.github.com>
2022-05-12 11:36:10 +0300

    try 1

---
+++ nsclient/main.c
@@ -0,0 +1,24 @@
+#include "ns_utils.h"
+
+int main() //do you know what is int main() ?
+{
+	unsigned char hostname[100];
+
+	RetrieveDnsServersFromRegistry();
+	
+	WSADATA firstsock;
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2,2),&firstsock) != 0)
+	{
+		printf("Failed. Error Code : %d",WSAGetLastError());
+		return 1;
+	}
+	printf("Initialised.");
+
+	printf("\nEnter Hostname to Lookup : ");
+	gets((char*)hostname);
+	ngethostbyname(hostname);
+
+	_getch();
+	return 0;
+}
--- nsclient/ns_utils.h
+++ nsclient/ns_utils.h
@@ -1,1 +1,16 @@
 #pragma once
+//Header Files
+#include "winsock2.h"
+#include "windows.h"
+#include "stdio.h"
+#include "conio.h"
+#include "structs.h"
+
+// #pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+//Function Declarations
+void ngethostbyname (unsigned char*);
+void ChangetoDnsNameFormat (unsigned char*,unsigned char*);
+unsigned char* ReadName (unsigned char*,unsigned char*,int*);
+void RetrieveDnsServersFromRegistry(void);
+unsigned char* PrepareDnsQueryPacket (unsigned char*);
