da2cd860e02c1556f65f87aefeb8778e36c468a7
Omri Elad <omlad93@gmail.com>
2022-05-13 10:39:15 +0300

    still working

--- nsclient/main.c
+++ nsclient/main.c
@@ -3,77 +3,73 @@
 #define MAX_LENGTH 2500
 
 int verify_domain_addr(char* addr) {
-	int i;
-	char c;
-	int len = strlen(addr);
-	//Check the domain address length
-	if (len < 2) {
-		printf("Domain address is too short\n");
-		return 1;
-	}
-	if (len > 63) {
-		printf("Domain address is too long\n");
-		return 1;
-	}
+    int i;
+    char c;
+    int len = strlen(addr);
+    // Check the domain address length
+    if (len < 2) {
+        printf("Domain address is too short\n");
+        return 1;
+    }
+    if (len > 63) {
+        printf("Domain address is too long\n");
+        return 1;
+    }
 
-	//Check all characters
-	for (i = 0; i < len; i++) {
-		c = addr[i];
-		if (((c > 'a') && (c < 'z')) || ((c > '0') && (c < '9')) || (c == '-') || (c == '.')) {
-			if (((i - 3) >= 0) && ((i + 1) < len)) {
-				if ((c == '-') && (addr[i - 3] == '.') && (addr[i + 1] == '-')) {
-					printf("The domain address does't meet domain address rouls\n");
-					return 1;
-				}
-			}
-		}
-	}
-	return 0;
+    // Check all characters
+    for (i = 0; i < len; i++) {
+        c = addr[i];
+        if (((c > 'a') && (c < 'z')) || ((c > '0') && (c < '9')) || (c == '-') || (c == '.')) {
+            if (((i - 3) >= 0) && ((i + 1) < len)) {
+                if ((c == '-') && (addr[i - 3] == '.') && (addr[i + 1] == '-')) {
+                    printf("The domain address does't meet domain address rouls\n");
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
 }
 
-int main(int argc, char* argv[]) //do you know what is int main() ?
+int ScanHostName(char* host) {
+    printf("\nPlease Enter Host Name:\n\t > ");
+    if (scanf("%s", host) == 0) {
+        printf("\n\tERROR: Failed Scanning Host Name\n");
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char* argv[])  // do you know what is int main() ?
 {
-	unsigned char hostname[100];
-	char domain[MAX_LENGTH];
-	int status;
-	char* ip = argv[1];
-	WSADATA firstsock;
+    unsigned char hostname[100];
+    char domain[MAX_LENGTH];
+    int status;
+    char* ip = argv[1];
+    WSADATA firstsock;
 
-	//RetrieveDnsServersFromRegistry();
+    // RetrieveDnsServersFromRegistry();
 
-	printf("\nInitialising Winsock...");
-	if (WSAStartup(MAKEWORD(2, 2), &firstsock) != 0)
-	{
-		printf("Failed. Error Code : %d", WSAGetLastError());
-		return 1;
-	}
-	printf("Initialised.");
+    printf("\nRunning nsclient for: %s", ip);
+    if (WSAStartup(MAKEWORD(2, 2), &firstsock) != 0) {
+        printf("\n\tERROR: Failed Initialising Winsock (%d)", WSAGetLastError());
+        return 1;
+    }
 
+    while (ScanHostName(domain)) {
+        // Runs Until Host Name Accepted
+    }
 
-	if (scanf("%s", domain) == 0) {
-		printf("ERROR IN SCANNING THE DOMAIN ADDRESS\n");
-		return 1;
-	}
+    while (strcmp(domain, "quit") != 0) {
+        // syntactic verification of the domain address
+        status = verify_domain_addr(domain);
+        if (status) {
+            printf("\n\t\tERROR: Received Bad Name");
+        } else {
+            GetHost(domain, ip);
+        }
+        ScanHostName(domain);
+    }
 
-	while (strcmp(domain, "quit") != 0) {
-		// syntactic verification of the domain address
-		status = verify_domain_addr(domain);
-		if (status) {
-			printf("BAD NAME\n");
-		}
-		else {
-			ngethostbyname(domain); // The function should also get the IP address that it is sending the query to
-		}
-		//scanning thr next domain address
-		if (scanf("%s", domain) == 0) {
-			printf("ERROR IN SCANNING THE DOMAIN ADDRESS\n");
-			return 1;
-		}
-	}
-
-	//gets((char*)hostname);
-	
-
-	//_getch();
-	return 0;
+    return 0;
 }
--- nsclient/ns_utils.h
+++ nsclient/ns_utils.h
@@ -1,16 +1,17 @@
 #pragma once
-//Header Files
-#include "winsock2.h"
-#include "windows.h"
-#include "stdio.h"
 #include "conio.h"
+#include "stdio.h"
 #include "structs.h"
+#include "windows.h"
+//#include "winsock2.h"
+//#pragma comment(lib, "Ws2_32.lib")
 
-// #pragma comment(lib,"ws2_32.lib") //Winsock Library
+// Function Declarations
+void GetHost(unsigned char* host, char* ip);
 
-//Function Declarations
-void ngethostbyname (unsigned char*);
-void ChangetoDnsNameFormat (unsigned char*,unsigned char*);
-unsigned char* ReadName (unsigned char*,unsigned char*,int*);
+unsigned char* ReadName(unsigned char*, unsigned char*, int*);
+
 void RetrieveDnsServersFromRegistry(void);
-unsigned char* PrepareDnsQueryPacket (unsigned char*);
+unsigned char* PrepareDnsQueryPacket(unsigned char*);
+
+void DnsFormat(unsigned char* dns, unsigned char* host);
